6913496382f0dede944aa1160766267b4bbd9eaa
Felix Delos Santos <f.delossantosiii1@nuigalway.ie>
2017-02-20 22:59:22 +0000

    Added training method

--- src/ConvNeuralNetwork/SarcasmCNN.py
+++ src/ConvNeuralNetwork/SarcasmCNN.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import json
-# import DataAcquisition.create_sarcasm_featuresets as dataprep
-# import create_sarcasm_featuresets as datahelp
-# from src.DataAcquisition import create_sarcasm_featuresets
-
-
+import sys
+sys.path.append('/Users/FelixDSantos/LeCode/DeepLearning/fyp/src/DataAcquisition')
+import create_sarcasm_featuresets as dataprep
+import time
 def loaddatafromjson(path):
     with open(path) as openfile:
         data = json.load(openfile)
@@ -21,7 +20,7 @@ num_classes= 2
 filter_sizes=[3,4,5]
 tweet_height=1
 num_filters=128
-fc_size= 64
+# fc_size= 64
 # ================================
 # ================================
 #PLACEHOLDERS
@@ -57,7 +56,7 @@ def create_conv_layer(input, num_channels,filter_sizes, num_filters, use_pooling
             convlayer = tf.nn.bias_add(convlayer, biases)
             reluresult = tf.nn.relu(convlayer, name="relu")
 
-            #we then maxpool our results
+            #we then maxpool our results tweet_length - filtersize+1
             maxpooled= tf.nn.max_pool(reluresult, ksize =[1, tweet_length - filtersize+1,1,1], strides=[1,1,1,1],padding ='VALID', name="maxpool")
 
             pooled_outputs.append(maxpooled)
@@ -75,10 +74,11 @@ def create_fully_connected_layer(input, num_inputs, num_outputs):
     Output=tf.nn.xw_plus_b(input, weights, bias, name = "Output")
     return Output
 
+# TODO REMOVE THIS
 xbatch1 , ybatch1 = train_x[1:5], train_y[1:5]
 
 convlayer1,num_feat1 = create_conv_layer(input=xreshaped, num_channels=num_channels,filter_sizes=filter_sizes, num_filters=num_filters, use_pooling=True)
-fclayer1 = create_fully_connected_layer(input=convlayer1, num_inputs=num_feat1, num_outputs=fc_size)
+fclayer1 = create_fully_connected_layer(input=convlayer1, num_inputs=num_feat1, num_outputs=num_classes)
 
 predictions = tf.argmax(fclayer1,1,name="Predictions")
 
@@ -87,25 +87,48 @@ with tf.name_scope("Cost"):
     cost = tf.reduce_mean(cross_ent)
 
 with tf.name_scope("Optimizer"):
-    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 with tf.name_scope("Accuracy"):
     correct_pred = tf.equal(predictions, tf.argmax(y,1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred,"float"),name="Accuracy")
 
+
+def train_network(data):
+    i=0
+     #start-time used for printing time-usage
+    start_time = time.time()
+    for batch in data:
+        xbatch, ybatch = zip(*batch)
+        feed_dict = {x:xbatch , y:ybatch}
+
+        _, acc=session.run([optimizer,accuracy], feed_dict=feed_dict)
+
+        i+=1
+        print("Step: ",i)
+        print("Accuracy: ", acc)
+        if(i%100==0):
+            print("\nEval:")
+            feed_dict_test = {x:test_x, y:test_y}
+            testacc=session.run(accuracy, feed_dict=feed_dict_test)
+            print("Accuracy: ", testacc)
+
+    # Ending time.
+    end_time = time.time()
+
+    # Difference between start and end-times.
+    time_dif = end_time - start_time
+    print("Time Used:",time_dif)
+
+
 session = tf.Session()
 session.run(tf.global_variables_initializer())
 
-feed_dict = {x:xbatch1, y:ybatch1}
-cls_pred = session.run(predictions, feed_dict=feed_dict)
-print(cls_pred)
+# feed_dict = {x:xbatch1, y:ybatch1}
+# cls_pred = session.run(predictions, feed_dict=feed_dict)
+# print(cls_pred)
+
+batches = dataprep.batch_iter(list(zip(train_x,train_y)), 200,200)
+train_network(batches)
 
 session.close()
-
-#TODO : Implement Train
-#TODO : Train in batches
-# def train(num_epochs):
-#
-#     start_time = time.time()
-#
-#     for i in range(num_epochs):
